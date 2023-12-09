388b12a4c07371c4587dc1da616aa7503931892a
Валерия <justlera007@gmail.com>
2023-12-09 19:02:18 +0300

    Разделение на компоненты

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import TripPresenter from './trip-presenter/trip-presenter.js';
+
+const filtersElement = document.querySelector('.trip-controls__filters');
+const tripEventsElement = document.querySelector('.trip-events');
+const tripMainElement = document.querySelector('.trip-main');
+
+const tripPresenter = new TripPresenter({
+  mainContainer: tripMainElement,
+  filterContainer: filtersElement,
+  tripContainer: tripEventsElement
+});
+
+tripPresenter.init();
